bc7533903ca5270f3874ce5806be4e48bcbb2607
admin <linxi@stent-tek.com>
2025-09-24 14:15:01 +0100

    deleted not in use files and folders

--- checkfile.py
+++
@@ -1,5 +0,0 @@
-import pefile
-
-pe = pefile.PE(r"C:\Program Files\Swift\Imaging\x64\swiftcam.dll")
-for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
-    print(exp.name)
--- modular code/main_entry_point.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from gui_application import AnalysisApp
-
-if __name__ == "__main__":
-    main_root = tk.Tk()
-    app = AnalysisApp(main_root)
-    main_root.mainloop()
