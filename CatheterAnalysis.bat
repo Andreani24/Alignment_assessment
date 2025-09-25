d7c37df458f509da004997e51424ecde67912f3e
admin <linxi@stent-tek.com>
2025-09-25 12:42:37 +0100

    25/09

--- CatheterAnalysis.bat
+++ CatheterAnalysis.bat
@@ -2,6 +2,6 @@
 cd /d "%~dp0"
 
 REM Run BetterGUI.py with the virtual environment's Python
-"%~dp0.venv\Scripts\python.exe" "%~dp0BetterGUI.py"
+"%~dp0.venv\Scripts\pythonw.exe" "%~dp0BetterGUI.py"
 
 pause
